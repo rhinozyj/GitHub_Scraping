6d48580b6a25ac0cbc8993b7603a8f72d2417df1
rhinozyj <yajingz@andrew.cmu.edu>
2022-06-23 17:47:34 -0400

    Update github_export.py

--- github_export.py
+++ github_export.py
@@ -6,7 +6,17 @@ Created on Thu Jun 16 10:14:37 2022
 """
 
 import pickle
+import time
 import csv
+import re
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import InvalidArgumentException
 
 ## Define Save and Read function for Pickle
 def save_obj(obj, saveFileName):
@@ -18,51 +28,68 @@ def load_obj(saveFileName):
     with open(saveFileName + ".pkl", "rb") as f:
         return pickle.load(f)
 
-microsoft_2 = load_obj("Microsoft_2")
-microsoft_3 = load_obj("Microsoft_3")
-microsoft = microsoft_2 + microsoft_3
+option = webdriver.ChromeOptions()
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
+## change company name
+apple = load_obj("apple")
 
 ## bg
 bg_dict_list = []
-for m in microsoft:
-    try:
-        profile_1 = m[6][0]
-    except:
-        profile_1 = ''
-    try:
-        profile_2 = m[6][1]
-    except:
-        profile_2 = ''
-    try:
-        profile_3 = m[6][2]
-    except:
-        profile_3 = ''
-    try:
-        profile_4 = m[6][3]
-    except:
-        profile_4 = ''
+for m in apple:        
+    linkedin = ''
+    email = ''
+    twitter = ''
+    other_profile = ''
+    for profile in m[6]:
+        if re.search('linkedin', profile) != None:
+            linkedin = profile
+        elif re.search('.@', profile) != None:
+            email = profile
+        elif re.search('^@', profile) != None:
+            twitter = profile
+        else:
+            if re.search('\.', profile) != None:
+                other_profile = profile 
+    if linkedin == '' and other_profile != '':
+        browser.get(other_profile) 
+        try:
+            linkedin = browser.find_elements(By.XPATH,".//a[contains(@href,'linkedin')]")[0].get_attribute("href")
+        except WebDriverException:
+            pass
+        except InvalidArgumentException:
+            try:
+                other_profile = "http://"+other_profile
+                linkedin = browser.find_elements(By.XPATH,".//a[contains(@href,'linkedin')]")[0].get_attribute("href")
+            except:
+                pass
+        except IndexError:
+            pass
+        finally:
+            time.sleep(2)
+            print(linkedin)
     bg_dict = {
-        'id': microsoft.index(m)+1,
+        'id': apple.index(m)+1,
         "github_link":''.join(m[0]),
         "name":''.join(m[1]),
         "username":''.join(m[2]),
-        "company":'Microsoft',
+        "company":'Apple',
         "location":''.join(m[4]),
         "years":m[5][0],
-        "profile_1":profile_1,
-        "profile_2":profile_2,
-        "profile_3":profile_3,
-        "profile_4":profile_4,
+        "linkedin":linkedin,
+        "email":email,
+        "twitter":twitter,
+        "other_profile":other_profile,
         "self_intro":''.join(m[7])}
     bg_dict_list.append(bg_dict)
 
 try:
-    with open("test_bg.csv", 'w', encoding="utf-8") as csvfile:
+    ## change file name
+    with open("apple_bg.csv", 'w', encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, ['id','github_link','name','username',
                                                 'company','location','years',
-                                                "profile_1","profile_2","profile_3",
-                                                "profile_4",'self_intro'], 
+                                                "linkedin","email","twitter",
+                                                "other_profile",'self_intro'], 
                                 lineterminator='\n')
         writer.writeheader()
         for data in bg_dict_list:
@@ -73,11 +100,11 @@ except IOError:
 
 # repo
 repo_dict_list = []
-for m in microsoft:
+for m in apple:
     for i in range(0,len(m[-5])):
         if len(m[-5][i])>0:
             repo_dict = {
-                'id': microsoft.index(m)+1,
+                'id': apple.index(m)+1,
                 "name":''.join(m[1]),
                 "username":''.join(m[2]),
                 "repo_id": i+1,
@@ -89,7 +116,8 @@ for m in microsoft:
             repo_dict_list.append(repo_dict)
 
 try:
-    with open("test_repo.csv", 'w', encoding="utf-8") as csvfile:
+    # change file name
+    with open("apple_repo.csv", 'w', encoding="utf-8") as csvfile:
         writer = csv.DictWriter(csvfile, ['id','name',"username","repo_id",'repo_link',
                                           'title','language','description','stars'], 
                                 lineterminator='\n')
@@ -100,60 +128,3 @@ except IOError:
     print("I/O error")
     
     
-# ## combined
-# combined_dict_list = []
-# for m in microsoft_2[:50]:
-#     try:
-#         profile_1 = m[6][0]
-#     except:
-#         profile_1 = ''
-#     try:
-#         profile_2 = m[6][1]
-#     except:
-#         profile_2 = ''
-#     try:
-#         profile_3 = m[6][2]
-#     except:
-#         profile_3 = ''
-#     try:
-#         profile_4 = m[6][3]
-#     except:
-#         profile_4 = ''
-#     for i in range(0,len(m[-5])):
-#         if len(m[-5][i])>0:
-#             combined_dict = {
-#                 'id': microsoft_2.index(m)+1,
-#                 "github_link":''.join(m[0]),
-#                 "name":''.join(m[1]),
-#                 "username":''.join(m[2]),
-#                 "company":'Microsoft',
-#                 "location":''.join(m[4]),
-#                 "years":m[5][0],
-#                 "profile_1":profile_1,
-#                 "profile_2":profile_2,
-#                 "profile_3":profile_3,
-#                 "profile_4":profile_4,
-#                 "self_intro":''.join(m[7]),
-#                 "repo_link":m[-5][i],
-#                 "title": m[-4][i],
-#                 "language": m[-3][i],
-#                 "description": m[-2][i],
-#                 "stars":m[-1][i]}
-#             combined_dict_list.append(combined_dict)
-
-# # print(repo_dict_list)
-
-# try:
-#     with open("test_combined.csv", 'w') as csvfile:
-#         writer = csv.DictWriter(csvfile, fieldnames= ['id','github_link','name',
-#                                                       'username','company','location',
-#                                                       'years',"profile_1","profile_2",
-#                                                       "profile_3","profile_4",'self_intro',
-#                                                       'repo_link','title','language',
-#                                                       'description','stars'], 
-#                                 lineterminator='\n')
-#         writer.writeheader()
-#         for data in combined_dict_list:
-#             writer.writerow(data)
-# except IOError:
-#     print("I/O error")
